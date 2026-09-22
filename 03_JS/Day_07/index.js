04110a48ea1f93b17e02e502c4fba60a38049a7c
Sameer Baral <baralsameer70@gmail.com>
2026-09-22 23:59:34 +0530

    day-07 array

---
+++ 03_JS/Day_07/index.js
@@ -0,0 +1,35 @@
+// Day-07       Array in JS
+
+
+let arr = [100,5.8,"sameer",70,80,90]
+// console.log(arr);
+// console.log(arr.length);
+// console.log(arr[2]);
+// console.log(typeof arr);  //object
+
+
+// updating the particular index element
+// arr[1] = 15
+// console.log(arr);
+
+
+// push an element at the end of the array
+// arr.push(200)
+// console.log(arr);
+
+
+// pop an element from the end of the array
+// arr.pop()
+// console.log(arr);
+
+
+// unshift: add an element at the start of the array
+// arr.unshift(10)
+// console.log(arr);
+
+
+// shift: remove an element from the start of the array
+// arr.shift()
+// console.log(arr);
+
+
